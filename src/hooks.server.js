ec08af0e00ad50d245c77b349b4d665939a0ff25
Heinsburg123 <heinsburg2005@gmail.com>
2024-09-20 17:06:12 -0400

    test db

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -4,11 +4,10 @@ export const handle = (async ({ event, resolve }) => {
   const dbconn = await connectToDB();
   event.locals = { dbconn };
   const query = {
-    text: 'INSERT INTO users(id,name,pass) VALUES($1, $2, $3)',
-    values: [10, 'c','c'],
+    text: 'SELECT * FROM users',
   }
   const res=await dbconn.query(query)
-  return new Response(res.rows[0])
+  return new Response(res.rows[0].name)
   const response = await resolve(event);
   dbconn.release();
   return response;
